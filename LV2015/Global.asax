d67bc8d16d11aea44187a0eb31c2a83d18827bb5
khoaht <hotankhoa.qn@gmail.com>
2015-01-03 12:38:29 +0700

    LV2015

---
+++ LV2015/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LV2015.MvcApplication" Language="C#" %>
---
+++ LV2015/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LV2015.Startup))]
+namespace LV2015
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
